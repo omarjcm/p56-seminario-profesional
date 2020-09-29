dc6f6d45d1124093eb37791aaf6724e3774204c3
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-09-28 19:41:31 -0500

    Se separa la parte de las respuestas por parte del servidor.

--- code/node-fundamentals/01_server/server.js
+++ code/node-fundamentals/01_server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const response = require('./network/response')
 
 const router = express.Router()
 
@@ -9,18 +10,15 @@ app.use( bodyParser.urlencoded( {extended:false} ) )
 app.use( router )
 
 router.get('/carrera', function(req, res) {
-    console.log( req.headers )
-
-    res.send( 'Lista de Carreras de la UPS.' )
+    response.success( req, res, 'Lista de Carreras de la UPS.', 200 )
 })
 router.post('/carrera', function(req, res) {
     if (req.query.error == 'ok') {
-        res.status(500).send( { tipo_error:1, mensaje_error:'Error en el Servidor.', mensaje_exito: '' } )
+        response.error( req, res, 'Error al ingresar la Carrera.', 500 )        
     } else {
-        res.status(200).send( { tipo_error:0, mensaje_error:'', mensaje_exito: 'Todo ok.' } )
-    }    
+        response.success( req, res, 'Ingreso de Carrera exitoso.', 201 )        
+    }
 })
-
 
 app.use( '/', express.static('public') )
 
